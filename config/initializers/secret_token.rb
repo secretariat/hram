d26cd41d0a4b52a5a11f76b73ed18518b47544db
«Secretariat1987» <mixey.viti@bigmir.net>
2013-11-02 23:31:44 +0200

    start commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hram::Application.config.secret_token = '52725368b2fa74cce7cbe140c524ff22e48daf0ca806774b776dfad71caf7139aa22d13b470dbedc6ade8bc09508f02ae149486500c1cfbe44b268dd63f30e85'
