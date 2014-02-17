f43abc8b7cccf331e5a2ca8023ca0f9e1ae73c08
ruslanpa <ruslan.pavlutskiy@gmail.com>
2014-02-17 18:11:34 +0200

    Adds refinery cms to existing application.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Hram
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.i18n.enforce_available_locales = true
   end
 end
