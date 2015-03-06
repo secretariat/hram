9fbc783511a4c5f5cc7a01c4b13791e2863a3b7e
Secretariat1987 <mixey.viti@bigmir.net>
2015-03-06 10:43:17 +0200

    some fix on index/ga/layout/title cd /home/mixey/ror/citrusdev/app/views/pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
 	def bool2str( bool )
     bool ? "Так" : "Ні"
   end
+
+	def meta_data(obj)
+    title obj.title
+    meta_d obj.meta_d
+    meta_k obj.meta_k
+  end
+
+  def meta_d(meta_d)
+    @meta_description = meta_d
+  end
+
+  def meta_k(meta_k)
+    @meta_keywords = meta_k
+  end
+
+  def title(title)
+    @meta_title = title
+  end
 end
